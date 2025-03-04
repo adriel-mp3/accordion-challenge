970f6259e104915f93e526dc12ffd34aebb2241c
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-04 15:28:12 -0300

    feat(views): create index

---
+++ src/views/IndexPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DefaultLayout from "@/layouts/DefaultLayout.vue";
+</script>
+
+<template>
+	<DefaultLayout>
+		<VBtn>Hello IndexPage</VBtn>
+	</DefaultLayout>
+</template>
