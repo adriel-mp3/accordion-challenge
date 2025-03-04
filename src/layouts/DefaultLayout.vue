800456b05f38c7e4fd9f54fc7deadc7f65793448
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-04 15:27:57 -0300

    feat(layouts): create default

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+	<v-layout>
+		<v-app-bar title="Explicaê"></v-app-bar>
+		<v-main class="flex items-center justify-center" style="min-height: 100vh">
+			<slot />
+		</v-main>
+	</v-layout>
+</template>
