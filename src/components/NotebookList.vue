ab18ff06c28f582f73f664e28ea44d277915c8c1
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 17:14:06 -0300

    feat: create checkboxes for topics

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -3,207 +3,29 @@ import { portugueseNotebooksWithoutCategory } from "@/mocks/notebook";
 import type { Category, Notebook } from "@/types";
 import { ref } from "vue";
 import SummaryDetails from "./SummaryDetails.vue";
+import TabSummaryHeader from "./TabSummaryHeader.vue";
+import { categories } from "@/mocks/category";
 
 const notebooks = ref<Omit<Notebook, "categories">[]>(
 	portugueseNotebooksWithoutCategory,
 );
-
-const notebookCategories = ref<{ [key: number]: Category[] }>({});
 const tab = ref(null);
-// Estado de loading para cada seção
+const notebookCategories = ref<{ [key: number]: Category[] }>({});
 const loading = ref<{ [key: number]: boolean }>({});
 
-// Simula requisição para buscar os conteúdos da seção pelo ID
 const getCategoriesByNotebookId = async (id: number) => {
-	if (notebookCategories.value[id]) return;
-	loading.value[id] = true;
+	try {
+		if (notebookCategories.value[id]) return;
+		loading.value[id] = true;
 
-	await new Promise((resolve) => setTimeout(resolve, 1500));
+		await new Promise((resolve) => setTimeout(resolve, 1500));
 
-	const mockData: { [key: number]: Category[] } = {
-		1: [
-			{
-				id: 1,
-				title: "Competência 01: Palavras, Imagem e Vida Social",
-				summary: {
-					exercisesCount: 100,
-					exercisesTime: "100h",
-					lessonsTime: "10h",
-					lessonsCount: 100,
-					materialsTime: "1h",
-					materialsCount: 10,
-				},
-				topics: [
-					{
-						id: 1,
-						title: "Titulo teste",
-						summary: {
-							exercisesCount: 100,
-							exercisesTime: "100h",
-							lessonsTime: "10h",
-							lessonsCount: 100,
-							materialsTime: "1h",
-							materialsCount: 10,
-						},
-						lessons: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								duration: "10h",
-								completed: true,
-							},
-						],
-						exercises: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								completed: true,
-							},
-						],
-						materials: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								completed: true,
-							},
-						],
-					},
-				],
-			},
-			{
-				id: 2,
-				title: "Competência 06: Texto - Contexto e Função",
-				summary: {
-					exercisesCount: 100,
-					exercisesTime: "100h",
-					lessonsTime: "10h",
-					lessonsCount: 100,
-					materialsTime: "1h",
-					materialsCount: 10,
-				},
-				topics: [
-					{
-						id: 1,
-						title: "Titulo testezin",
-						summary: {
-							exercisesCount: 100,
-							exercisesTime: "100h",
-							lessonsTime: "10h",
-							lessonsCount: 100,
-							materialsTime: "1h",
-							materialsCount: 10,
-						},
-						lessons: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								duration: "10h",
-								completed: true,
-							},
-						],
-						exercises: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								completed: true,
-							},
-						],
-						materials: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								completed: true,
-							},
-						],
-					},
-					{
-						id: 1,
-						title: "Titulo testezin 2",
-						summary: {
-							exercisesCount: 100,
-							exercisesTime: "100h",
-							lessonsTime: "10h",
-							lessonsCount: 100,
-							materialsTime: "1h",
-							materialsCount: 10,
-						},
-						lessons: [
-							{
-								id: 1,
-								title: "Lição 1",
-								duration: "10h",
-								completed: true,
-							},
-              {
-								id: 1,
-								title: "Lição 2",
-								duration: "10h",
-								completed: true,
-							},
-						],
-						exercises: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								completed: true,
-							},
-						],
-						materials: [
-							{
-								id: 1,
-								title: "Titulo teste",
-								completed: true,
-							},
-						],
-					},
-				],
-			},
-			{
-				id: 3,
-				title: "Competência 07: Opiniões e Pontos de Vista",
-				summary: {
-					exercisesCount: 100,
-					exercisesTime: "100h",
-					lessonsTime: "10h",
-					lessonsCount: 100,
-					materialsTime: "1h",
-					materialsCount: 10,
-				},
-				topics: [],
-			},
-		],
-		2: [
-			{
-				id: 1,
-				title: "Fonologia",
-				summary: {
-					exercisesCount: 100,
-					exercisesTime: "100h",
-					lessonsTime: "10h",
-					lessonsCount: 100,
-					materialsTime: "1h",
-					materialsCount: 10,
-				},
-				topics: [],
-			},
-			{
-				id: 1,
-				title: "Morfologia",
-				summary: {
-					exercisesCount: 100,
-					exercisesTime: "100h",
-					lessonsTime: "10h",
-					lessonsCount: 100,
-					materialsTime: "1h",
-					materialsCount: 10,
-				},
-				topics: [],
-			},
-		],
-	};
-
-	notebookCategories.value[id] = mockData[id] || [];
-	loading.value[id] = false;
+		notebookCategories.value[id] = categories[id] || [];
+	} catch (e) {
+		console.error("Erro ao carregar categorias:", e);
+	} finally {
+		loading.value[id] = false;
+	}
 };
 </script>
 
@@ -216,9 +38,11 @@ const getCategoriesByNotebookId = async (id: number) => {
 					:key="notebook.id"
 					@group:selected="getCategoriesByNotebookId(notebook.id)"
 				>
-					<v-expansion-panel-title class="!block">
-						<p class="mb-2 font-bold">{{ notebook.title }}</p>
-						<SummaryDetails :summary="notebook.summary" />
+					<v-expansion-panel-title>
+						<div>
+							<p class="mb-2 font-bold">{{ notebook.title }}</p>
+							<SummaryDetails :summary="notebook.summary" />
+						</div>
 					</v-expansion-panel-title>
 
 					<v-expansion-panel-text>
@@ -237,9 +61,11 @@ const getCategoriesByNotebookId = async (id: number) => {
 									v-for="category in notebookCategories[notebook.id]"
 									:key="category.id"
 								>
-									<v-expansion-panel-title class="!block !bg-neutral-100">
-										<p class="mb-2 font-bold">{{ category.title }}</p>
-										<SummaryDetails :summary="category.summary" />
+									<v-expansion-panel-title class="!bg-neutral-100">
+										<div>
+											<p class="mb-2 font-bold">{{ category.title }}</p>
+											<SummaryDetails :summary="category.summary" />
+										</div>
 									</v-expansion-panel-title>
 
 									<v-expansion-panel-text>
@@ -249,9 +75,11 @@ const getCategoriesByNotebookId = async (id: number) => {
 												v-for="topic in category.topics"
 												:key="topic.id"
 											>
-												<v-expansion-panel-title class="!block">
-													<p class="mb-2 font-bold">{{ topic.title }}</p>
-													<SummaryDetails :summary="topic.summary" />
+												<v-expansion-panel-title>
+													<div>
+														<p class="mb-2 font-bold">{{ topic.title }}</p>
+														<SummaryDetails :summary="topic.summary" />
+													</div>
 												</v-expansion-panel-title>
 
 												<v-expansion-panel-text>
@@ -268,7 +96,7 @@ const getCategoriesByNotebookId = async (id: number) => {
 															value="exercises"
 															class="!text-xs !normal-case"
 														>
-															Exercícios{{ topic.summary.exercisesCount }} |
+															Exercícios {{ topic.summary.exercisesCount }} |
 															{{ topic.summary.exercisesTime }}
 														</v-tab>
 														<v-tab
@@ -283,22 +111,89 @@ const getCategoriesByNotebookId = async (id: number) => {
 													<v-card-text>
 														<v-tabs-window v-model="tab">
 															<v-tabs-window-item value="lessons">
+																<TabSummaryHeader
+																	label="Aulas"
+																	:title="topic.leassonType"
+																	:summary="{
+																		count: topic.summary.lessonsCount,
+																		time: topic.summary.lessonsTime,
+																	}"
+																/>
 																<ul>
 																	<li
 																		v-for="lesson in topic.lessons"
 																		:key="lesson.id"
+																		class="flex items-center gap-2"
 																	>
-																		{{ lesson.title }}
+																		<v-checkbox-btn
+																			color="deep-purple-accent-4"
+																			density="compact"
+																			v-model="lesson.completed"
+																			:label="lesson.title"
+																		/>
+																		<span class="text-sm text-neutral-400">
+																			{{ lesson.duration }}
+																		</span>
 																	</li>
 																</ul>
 															</v-tabs-window-item>
 
 															<v-tabs-window-item value="exercises">
-																Two
+																<TabSummaryHeader
+																	class="mb-2"
+																	label="Exercícios"
+																	:title="topic.exerciseType"
+																	:summary="{
+																		count: topic.summary.exercisesCount,
+																		time: topic.summary.exercisesTime,
+																	}"
+																/>
+																<ul>
+																	<li
+																		v-for="exercise in topic.exercises"
+																		:key="exercise.id"
+																		class="flex items-center gap-2"
+																	>
+																		<v-checkbox-btn
+																			density="compact"
+																			color="deep-purple-accent-4"
+																			v-model="exercise.completed"
+																			:label="exercise.title"
+																		/>
+																		<span class="text-sm text-neutral-400">
+																			{{ exercise.duration }}
+																		</span>
+																	</li>
+																</ul>
 															</v-tabs-window-item>
 
 															<v-tabs-window-item value="materials">
-																Three
+																<TabSummaryHeader
+																	class="mb-2"
+																	label="Materiais"
+																	:title="topic.materialType"
+																	:summary="{
+																		count: topic.summary.materialsCount,
+																		time: topic.summary.materialsTime,
+																	}"
+																/>
+																<ul>
+																	<li
+																		v-for="material in topic.materials"
+																		class="flex items-center gap-2"
+																		:key="material.id"
+																	>
+																		<v-checkbox-btn
+																			density="compact"
+																			v-model="material.completed"
+																			color="deep-purple-accent-4"
+																			:label="material.title"
+																		/>
+																		<span class="text-sm text-neutral-400">
+																			{{ material.duration }}
+																		</span>
+																	</li>
+																</ul>
 															</v-tabs-window-item>
 														</v-tabs-window>
 													</v-card-text>
