630f49b755d3c31b159600429fdab8399bfd0dcd
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 03:45:08 -0300

    feat(components): create breadcrumbs

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+interface BreadCrumbItems {
+	title: string;
+	disabled?: boolean;
+	href: string;
+	active?: boolean;
+}
+
+defineProps<{
+  items: BreadCrumbItems[];
+}>();
+</script>
+
+<template>
+	<div>
+		<v-breadcrumbs
+			:items="items"
+			divider="/"
+			active-class="!underline !decoration-purple-600 !decoration-3 !underline-offset-8"
+		></v-breadcrumbs>
+	</div>
+</template>
