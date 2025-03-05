eb341d09e22405f5c239d464082876172a889f72
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 03:47:57 -0300

    feat: create index page

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import BreadCrumb from "@/components/BreadCrumb.vue";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
+import NotebookList from "@/components/NotebookList.vue";
+
+const breadcrumbItems = ref([
+	{
+		title: "Cadernos",
+		disabled: true,
+		href: "#",
+	},
+	{
+		title: "Língua Portuguesa",
+		disabled: false,
+		href: "#",
+		active: true,
+	},
+]);
 </script>
 
 <template>
 	<DefaultLayout>
-		<VBtn>Hello IndexPage</VBtn>
+		<BreadCrumb :items="breadcrumbItems" class="mb-8" />
+		<VContainer>
+			<NotebookList />
+		</VContainer>
 	</DefaultLayout>
 </template>
