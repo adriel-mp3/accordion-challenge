bfb6109ed169be2af1e0b7f172efcbd0a179cef1
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 03:46:55 -0300

    feat(components): create summary details

---
+++ src/components/SummaryDetails.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { CategorySummary } from "@/types";
+
+defineProps<{ summary: CategorySummary }>();
+</script>
+
+<template>
+	<ul class="flex flex-wrap gap-4 text-xs text-neutral-500">
+		<li>
+			<span class="font-bold text-neutral-700">Aulas</span>
+			{{ summary.lessonsCount }} | {{ summary.lessonsTime }}
+		</li>
+		<li>
+			<span class="font-bold text-neutral-700">Exercícios</span>
+			{{ summary.exercisesCount }} | {{ summary.exercisesTime }}
+		</li>
+		<li>
+			<span class="font-bold text-neutral-700">Materiais</span>
+			{{ summary.materialsCount }} | {{ summary.materialsTime }}
+		</li>
+	</ul>
+</template>
