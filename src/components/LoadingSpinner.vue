01fefb3a7a1df38ce20d0c868136fff8d2ad3e98
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 18:12:57 -0300

    feat(components): create loading spoinner

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+	message: {
+		type: String,
+		default: "Carregando...",
+	},
+});
+</script>
+<template>
+	<div class="flex w-full items-center justify-center gap-4 py-4">
+		<v-progress-circular indeterminate color="purple" />
+		<span>{{ message }}</span>
+	</div>
+</template>
