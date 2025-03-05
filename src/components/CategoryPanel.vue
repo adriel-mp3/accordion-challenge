3eb95b0b89cc3338bac9602030927849df4cace7
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 18:13:13 -0300

    refactor: import loading spinner

--- src/components/CategoryPanel.vue
+++ src/components/CategoryPanel.vue
@@ -3,26 +3,26 @@ import { useStore } from "vuex";
 import { computed } from "vue";
 import SummaryDetails from "@/components/SummaryDetails.vue";
 import TopicPanel from "@/components/TopicPanel.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 
 const props = defineProps<{ notebookId: number }>();
 const store = useStore();
 const getCategoriesByNotebook = computed(() =>
-	store.getters.getCategoriesByNotebook(props.notebookId)
+	store.getters.getCategoriesByNotebook(props.notebookId),
 );
 const getLoadingState = computed(() =>
-	store.getters.getLoadingState(props.notebookId)
+	store.getters.getLoadingState(props.notebookId),
 );
 </script>
 
 <template>
-	<div v-if="getLoadingState" class="flex w-full items-center justify-center gap-4 py-4">
-		<v-progress-circular indeterminate color="purple" />
-		<span>Carregando...</span>
-	</div>
-
+	<LoadingSpinner v-if="getLoadingState" />
 	<template v-else>
 		<v-expansion-panels>
-			<v-expansion-panel v-for="category in getCategoriesByNotebook" :key="category.id">
+			<v-expansion-panel
+				v-for="category in getCategoriesByNotebook"
+				:key="category.id"
+			>
 				<v-expansion-panel-title class="!bg-neutral-100">
 					<div>
 						<p class="mb-2 font-bold">{{ category.title }}</p>
