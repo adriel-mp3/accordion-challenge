716bb3763de4a500f3362cb0a83256f13f42f9a9
adriel-mp3 <contatoadrielsantos@hotmail.com>
2025-03-05 03:47:16 -0300

    feat(mocks): create portuguese notebook mock

---
+++ src/mocks/notebook.ts
@@ -0,0 +1,26 @@
+export const portugueseNotebooksWithoutCategory = [
+	{
+		id: 1,
+		title: "Interpretação de Textos",
+		summary: {
+			exercisesCount: 100,
+			exercisesTime: "100h",
+			lessonsTime: "10h",
+			lessonsCount: 100,
+			materialsTime: "1h",
+			materialsCount: 10,
+		},
+	},
+	{
+		id: 2,
+		title: "Gramática",
+		summary: {
+			exercisesCount: 100,
+			exercisesTime: "100h",
+			lessonsTime: "10h",
+			lessonsCount: 100,
+			materialsTime: "1h",
+			materialsCount: 10,
+		},
+	},
+];
